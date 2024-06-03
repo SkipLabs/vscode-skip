b80a188735d7b077b98ca8fc355e1c8cdc93a57c
Mehdi Bouaziz <mehdi@skiplabs.io>
2024-06-03 13:38:46 +0200

    Format and new version

--- src/extension.ts
+++ src/extension.ts
@@ -33,7 +33,8 @@ const documentSymbolProvider = {
   provideDocumentSymbols(
     document: vscode.TextDocument,
   ): vscode.SymbolInformation[] {
-    const symbolRE = /(?<!\/\/\s*)(?:\b(?<kind1>class|const|fun|module|trait|type)\s+\.?(?<name1>\w+)|(?<kind2>\|)\s*\.?(?<name2>\w+)\s*\((?!.+->))/dg;
+    const symbolRE =
+      /(?<!\/\/\s*)(?:\b(?<kind1>class|const|fun|module|trait|type)\s+\.?(?<name1>\w+)|(?<kind2>\|)\s*\.?(?<name2>\w+)\s*\((?!.+->))/dg;
 
     const results = [];
     let currentModule = undefined;
@@ -46,10 +47,10 @@ const documentSymbolProvider = {
         continue;
       }
 
-      const rawKind = groups['kind1'] ?? groups['kind2'];
-      const name = groups['name1'] ?? groups['name2'];
-      const nameIndices = indices['name1'] ?? indices['name2'];
-      if (rawKind === 'module' && name === 'end') {
+      const rawKind = groups["kind1"] ?? groups["kind2"];
+      const name = groups["name1"] ?? groups["name2"];
+      const nameIndices = indices["name1"] ?? indices["name2"];
+      if (rawKind === "module" && name === "end") {
         currentModule = undefined;
         currentClass = undefined;
         continue;
@@ -59,47 +60,72 @@ const documentSymbolProvider = {
         continue;
       }
 
-      const kind = (rawKind === 'class' || rawKind === '|') ? vscode.SymbolKind.Class :
-        rawKind === 'const' ? vscode.SymbolKind.Constant :
-          rawKind === 'fun' ? (currentClass !== undefined ? vscode.SymbolKind.Method : vscode.SymbolKind.Function) :
-            rawKind === 'module' ? vscode.SymbolKind.Module :
-              rawKind === 'trait' ? vscode.SymbolKind.Class :
-                rawKind === 'type' ? vscode.SymbolKind.Struct : undefined;
+      const kind =
+        rawKind === "class" || rawKind === "|"
+          ? vscode.SymbolKind.Class
+          : rawKind === "const"
+            ? vscode.SymbolKind.Constant
+            : rawKind === "fun"
+              ? currentClass !== undefined
+                ? vscode.SymbolKind.Method
+                : vscode.SymbolKind.Function
+              : rawKind === "module"
+                ? vscode.SymbolKind.Module
+                : rawKind === "trait"
+                  ? vscode.SymbolKind.Class
+                  : rawKind === "type"
+                    ? vscode.SymbolKind.Struct
+                    : undefined;
       if (kind === undefined) {
         continue;
       }
       // TODO: the range should actually be the range of the module, class, function, etc... not of the symbol name
-      const range = new vscode.Range(document.positionAt(nameIndices[0]), document.positionAt(nameIndices[1]));
+      const range = new vscode.Range(
+        document.positionAt(nameIndices[0]),
+        document.positionAt(nameIndices[1]),
+      );
       const location = new vscode.Location(document.uri, range);
-      const containerName = currentClass ?? currentModule ?? '';
-      results.push(new vscode.SymbolInformation(name, kind, containerName, location));
-      if (rawKind === 'module') {
+      const containerName = currentClass ?? currentModule ?? "";
+      results.push(
+        new vscode.SymbolInformation(name, kind, containerName, location),
+      );
+      if (rawKind === "module") {
         currentModule = name;
         currentClass = undefined;
-      } else if (rawKind === 'class' || rawKind === 'trait') {
+      } else if (rawKind === "class" || rawKind === "trait") {
         currentClass = name;
       }
     }
     return results;
-  }
+  },
 };
 
-const addDefinitionsInDocument = (add: (loc: vscode.Location) => boolean, document: vscode.TextDocument, word: string, token: vscode.CancellationToken): boolean => {
-  const symbolRE = new RegExp(`(?<!\\/\\/\\s*)(?:\\b(?:class|const|fun|module|trait|type)\\s+\\.?${word}\\b|\\|\\s*\\.?${word}\\s*\\((?!.+->))`, 'dg');
+const addDefinitionsInDocument = (
+  add: (loc: vscode.Location) => boolean,
+  document: vscode.TextDocument,
+  word: string,
+  token: vscode.CancellationToken,
+): boolean => {
+  const symbolRE = new RegExp(
+    `(?<!\\/\\/\\s*)(?:\\b(?:class|const|fun|module|trait|type)\\s+\\.?${word}\\b|\\|\\s*\\.?${word}\\s*\\((?!.+->))`,
+    "dg",
+  );
   const text = document.getText();
   let match;
   while ((match = symbolRE.exec(text)) !== null) {
-    if (token.isCancellationRequested)
-      return false;
+    if (token.isCancellationRequested) return false;
     const pos = document.positionAt(match.index);
-    if (!add(new vscode.Location(document.uri, pos)))
-      return false;
+    if (!add(new vscode.Location(document.uri, pos))) return false;
   }
   return true;
-}
+};
 
 const documentDefinitionProvider = {
-  async provideDefinition(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken): Promise<vscode.Location[]> {
+  async provideDefinition(
+    document: vscode.TextDocument,
+    position: vscode.Position,
+    token: vscode.CancellationToken,
+  ): Promise<vscode.Location[]> {
     const range = document.getWordRangeAtPosition(position);
     if (range === undefined) {
       return [];
@@ -107,29 +133,34 @@ const documentDefinitionProvider = {
     const MAX_RESULTS = 100;
     const MAX_FILES_TO_SEARCH = 200;
     const inTests = /(^|[/])tests[/]/.test(document.fileName);
-    const exclude = inTests ? '**/target/**' : '**/{target,tests}/**';
-    const files = await vscode.workspace.findFiles('**/*.sk', exclude, MAX_FILES_TO_SEARCH, token);
+    const exclude = inTests ? "**/target/**" : "**/{target,tests}/**";
+    const files = await vscode.workspace.findFiles(
+      "**/*.sk",
+      exclude,
+      MAX_FILES_TO_SEARCH,
+      token,
+    );
     const word = document.getText(range);
 
-    if (token.isCancellationRequested)
-      return [];
+    if (token.isCancellationRequested) return [];
 
     const results: vscode.Location[] = [];
-    const add = (loc: vscode.Location): boolean => { results.push(loc); return (results.length <= MAX_RESULTS); };
+    const add = (loc: vscode.Location): boolean => {
+      results.push(loc);
+      return results.length <= MAX_RESULTS;
+    };
     if (addDefinitionsInDocument(add, document, word, token)) {
       for (const file of files) {
-        if (token.isCancellationRequested)
-          break;
+        if (token.isCancellationRequested) break;
         if (file === document.uri) {
           continue;
         }
         const fileDocument = await vscode.workspace.openTextDocument(file);
-        if (!addDefinitionsInDocument(add, fileDocument, word, token))
-          break;
+        if (!addDefinitionsInDocument(add, fileDocument, word, token)) break;
       }
     }
     return results;
-  }
+  },
 };
 
 export function activate(context: vscode.ExtensionContext) {
@@ -137,22 +168,25 @@ export function activate(context: vscode.ExtensionContext) {
 
   const skipBuffers = { language: "skip" };
 
-  const formatterDisposable = vscode.languages.registerDocumentFormattingEditProvider(
-    skipBuffers,
-    documentFormattingEditProvider,
-  );
+  const formatterDisposable =
+    vscode.languages.registerDocumentFormattingEditProvider(
+      skipBuffers,
+      documentFormattingEditProvider,
+    );
   context.subscriptions.push(formatterDisposable);
 
-  const symbolProviderDisposable = vscode.languages.registerDocumentSymbolProvider(
-    skipBuffers,
-    documentSymbolProvider,
-  );
+  const symbolProviderDisposable =
+    vscode.languages.registerDocumentSymbolProvider(
+      skipBuffers,
+      documentSymbolProvider,
+    );
   context.subscriptions.push(symbolProviderDisposable);
 
-  const definitionProviderDisposable = vscode.languages.registerDefinitionProvider(
-    skipBuffers,
-    documentDefinitionProvider,
-  );
+  const definitionProviderDisposable =
+    vscode.languages.registerDefinitionProvider(
+      skipBuffers,
+      documentDefinitionProvider,
+    );
   context.subscriptions.push(definitionProviderDisposable);
 }
 
